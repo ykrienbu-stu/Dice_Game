34644590eb548f1bf65ffcdea0edd07a16d34e1f
Young-Do Krienbuehl <ykrienbu@terpmail.umd.edu>
2024-11-27 23:34:06 -0500

    4th commit, attempt to add "tuple out" to rounds

--- Dice_Game2.py
+++ Dice_Game2.py
@@ -3,85 +3,94 @@
 import random
 
 #Introduce the dice Game and display the rules
-'''
+print('''
 Hello! Welcome to the 'Tuple Out' Dice Game!!!
 This is a game where you roll dice to get the hightest score.
 You will be playing against a computer to see who gets the highest score
 after a set amount of rounds.
-'''
+''')
 # Ask for the amount of rounds the user would like to play
 try:
-   rounds = int(input("How many rounds would you like to play?"))
+   rounds = int(input("How many rounds would you like to play?: "))
 except ValueError:
-   print("Invalid input. Please input a number.")
+   print("Invalid input. Exiting program.")
    exit()
 
 
 # Load previous save file if there is one 
+# Need to work on this feature
 # with open("scores.txt", "r") as file:
 
 
-# Initialize variables
-player_dice = (0, 0, 0)
-cpu_dice = (0, 0, 0)
-player_score = 0
-cpu_score = 0
-
 # Represents the sides of the dice.
 sides = [1,2,3,4,5,6]
+#
+player_total_score = 0
+cpu_total_score = 0
 
 
-# Test the random dice
-# dice1 = random.choice(sides)
-# dice2 = random.choice(sides)
-# dice3 = random.choice(sides)
-# print( dice1, "\n", dice2, "\n", dice3)
+
+for round_num in range(1, rounds + 1):
+   # Start of each round
+   print(f"\n------ Round {round_num} ------")
+
+   # Player Turn
+   response = input("Are you ready!!!\nType 'R' to roll dice: ")
+   if response.casefold() == "r":
+      dice1 = random.choice(sides)
+      dice2 = random.choice(sides)
+      dice3 = random.choice(sides)
+
+      #Tuple out condition
+      if dice1 == dice2 == dice3:
+            player_round_score = 0
+            print(f"\nYou rolled {dice1}, {dice2}, {dice3}.\nCurrent score: {player_round_score}")
+      else:
+         player_round_score = dice1 + dice2 + dice3
+         print(f"\nYou rolled {dice1}, {dice2}, {dice3}.\nCurrent score: {player_round_score}")
+
+      # Add round score to total score   
+      player_total_score += player_round_score   
+   else: 
+      print("U didnt type 'R' Exiting program") 
+      exit()
 
 
-def play_game(rounds):
+   #Cpu Turn
+   cpu_dice1 = random.choice(sides)
+   cpu_dice2 = random.choice(sides)
+   cpu_dice3 = random.choice(sides)
 
-   for round_num in range(1, rounds):
-      print(f"\n------ Round {round_num} ------")
-
-      # Player Turn
-      response = input("Are you ready!!!\nType 'R' to roll dice")
-      if response.casefold() == "r":
-         dice1 = random.choice(sides)
-         dice2 = random.choice(sides)
-         dice3 = random.choice(sides) 
-         current_player_score = dice1 + dice2 + dice3
-         print(f"You rolled {dice1},{dice2},{dice3}.\nCurrent score: {current_player_score}")
-      else: 
-         print("U didnt type 'R' please type 'R' to roll the dice") 
-         exit()
-      #Cpu Turn
-      cpu_dice1 = random.choice(sides)
-      cpu_dice2 = random.choice(sides)
-      cpu_dice3 = random.choice(sides)
-      current_cpu_score = cpu_dice1 + cpu_dice2 + cpu_dice3
-      print(f"The CPU rolled {cpu_dice1},{cpu_dice2},{cpu_dice3}.\nCurrent score: {current_cpu_score}")
-      #End of Round, display current scores
-      print(f"End of Round 1\nPlayer Score: {player_score}\nCPU score: {cpu_score}")
-
-
-   return player_score, cpu_score #return with player scores
-
+   # Tuple out condition
+   if cpu_dice1 == cpu_dice2 == cpu_dice3: 
+      print(f"\nThe CPU rolled {cpu_dice1}, {cpu_dice2}, {cpu_dice3}. Tuple Out! No points this round.")
+      cpu_round_score = 0
+   else:
+      cpu_round_score = cpu_dice1 + cpu_dice2 + cpu_dice3
+      print(f"\nThe CPU rolled {cpu_dice1}, {cpu_dice2}, {cpu_dice3}.\nCurrent score: {cpu_round_score}")
+   
+   # Add round score to total score
+   cpu_total_score += cpu_round_score
+   
+print(f"{player_total_score} {cpu_total_score}")   
 
 
 ########################################################################################
 #Game End
 #Declare winner when set amount of rounds has been reached
 
-
-# Declare the winner
 print("\n--- Game Over ---")
-if player_score > cpu_score:
-   print(f"Congratulations! You won with a score of {player_score} to {cpu_score}.")
-elif cpu_score > player_score:
-   print(f"CPU wins with a score of {cpu_score} to {player_score}. Better luck next time!")
+if player_total_score > cpu_total_score:
+   print(f"Congratulations! You won with a score of {player_total_score} to {cpu_total_score}.")
+elif cpu_total_score > player_total_score:
+   print(f"CPU wins with a score of {cpu_total_score} to {player_total_score}. Better luck next time!")
 else:
-   print(f"It's a tie! Both scored {player_score}. Well played!")
+   print(f"It's a tie! Both scored {player_total_score}. Well played!")
 
 # Save scores to file
 with open("scores.txt", "w") as file:
-   file.write()
+   file.write(f"Final Player Score: {player_total_score}\n")
+   file.write(f"Final Opponent Score: {cpu_total_score}\n")
+   file.write(f"Rounds played: {rounds}\n")
+# Thank you message to player
+print("\n\nThanks for playing my game!!!\n-Young-Do Krienbuehl")
